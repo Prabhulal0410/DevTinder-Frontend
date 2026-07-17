34db8d1c51e0ed269b951e8e36e95bf20cedea0b
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-17 22:39:11 +0530

    craetwd outlet for bodys children component

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
 
 const Body = () => {
   return (
     <div>
         <Navbar/>
+        <Outlet/>
     </div>
   )
 }
