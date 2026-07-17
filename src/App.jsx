085551fe3ada5c72ef41ee12134380831ca273f2
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-17 22:37:27 +0530

    add routing and create login and profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./Navbar"
+import Body from "./Body"
+import Login from "./Login"
+import Profile from "./Profile"
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
       <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-5xl font-bold text-cyan-400">
         DevTinder 🚀
       </h1>
-      <button class="btn btn-primary">Button</button>
     </div>
     </>
   )
