8e23ad5525c4f2e8fcad1a3f82a6b9b4e35fe1a7
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-07-17 22:52:54 +0530

    added footer component and aded in body component

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,6 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-
-
-      <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-cyan-400">
-        DevTinder 🚀
-      </h1>
-    </div>
     </>
   )
 }
